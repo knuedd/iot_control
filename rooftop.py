163895197ecefbae6a7cf77ace2a9927dbe19ca3
Andreas Knüpfer <knue@posteo.de>
2022-01-16 18:31:13 +0000

    Debug level "info"

--- rooftop.py
+++ rooftop.py
@@ -11,6 +11,7 @@ from iot_control.iotruntime import IoTRuntime
 if __name__ == '__main__':
 
     # Creates a runtime that will run everything
-    runtime = IoTRuntime("setup.yaml", logging.DEBUG)
+    #runtime = IoTRuntime("setup.yaml", logging.DEBUG)
+    runtime = IoTRuntime("setup.yaml", logging.INFO)
     runtime.set_intervall(60)
     runtime.loop_forever()
